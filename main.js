caaafe70a839edc24d32ae5308509382edd3b79c
SuperHeroCoder <88969705+SuperHeroCoder@users.noreply.github.com>
2022-04-04 18:21:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,15 +25,15 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded() { console.log('Model Loaded!'); }
 
 function check() {
-    img = document.getElementById('selfie_image');
-    classifier.classify(img, gotResult);
-}
-
-function gotResult(error, results) {
-    // Display error in the console
-    if (error) { console.error(error); } else {
-        console.log(results);
-        document.getElementById("result_object_name").innerHTML = results[0].label;
-        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    img = document.getElementById('      
+        classifier.classify(img, gotResult);
     }
-}
+
+    function gotResult(error, results) {
+        // Display error in the console
+        if (error) { console.error(error); } else {
+            console.log(results);
+            document.getElementById("result_object_name").innerHTML = results[0].label;
+            document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        }
+    }
